037038b0afd24082bf6994ac9af4e70f048ff301
AsiG <Asi.g100@gmail.com>
2019-09-07 19:41:14 +0300

    add more documentation

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ $('.add-radio').on('click', function(){
     addSwitch();
 });
 
+//clears the revent display
 $('#clear').on('click', function(){
     $('#res').html('');
 });
 
+//switch between add package and add item
 function addSwitch(){
     state = $('.add-radio:checked').val();
     
